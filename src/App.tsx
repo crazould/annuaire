71cd1d0df511434306cb1e551cda5090a068934a
crazould <53102589+crazould@users.noreply.github.com>
2022-09-30 06:34:57 +0700

    add base dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ContactList from "./component/ContactList";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import FormContact from "./component/FormContact";
 
 function App() {
+  const nav = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Phone book</h1>
+      <Routes>
+        <Route path="/" element={<ContactList nav={nav} />} />
+        <Route path="/form" element={<FormContact nav={nav} />} />
+      </Routes>
     </div>
   );
 }
