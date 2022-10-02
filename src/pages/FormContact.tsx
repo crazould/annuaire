cbf8e9fb82a2678dd9f66f9bb781979802018d7c
crazould <53102589+crazould@users.noreply.github.com>
2022-10-02 10:33:03 +0700

    add number input

--- src/pages/FormContact.tsx
+++ src/pages/FormContact.tsx
@@ -1,63 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import useGetContactDetail from "../hooks/useGetContactDetail";
-import { Contact, Phone } from "./ContactList";
+import { Phone } from "./ContactList";
 
 const FormContact = () => {
   const { id } = useParams();
   const { loading, error, data } = useGetContactDetail(parseInt(id as string));
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phones, setPhones] = useState([{ number: "" }]);
+
   if (id && (loading || error)) return <>...</>;
 
-  const handleChange = () => console.log("");
-  let contact: Contact = {
-    id: "",
-    first_name: "",
-    last_name: "",
-    phones: [{ number: "" }],
+  const changeFirstName = (e: React.ChangeEvent) => {
+    const input = e.target as HTMLInputElement;
+    setFirstName(input.value);
+  };
+
+  const changeLastName = (e: React.ChangeEvent) => {
+    const input = e.target as HTMLInputElement;
+    setLastName(input.value);
+  };
+
+  const changePhone = (e: React.ChangeEvent, idx: number) => {
+    const input = e.target as HTMLInputElement;
+    let newPhones = [...phones];
+    newPhones[idx].number = input.value;
+    setPhones(newPhones);
   };
 
   if (data) {
-    Object.assign(contact, data.contact_by_pk);
+    const { first_name, last_name, phones } = data.contact_by_pk;
+    setFirstName(first_name);
+    setLastName(last_name);
+    setPhones(phones);
   }
 
-  const { first_name, last_name, phones } = contact;
-  const phoneList = phones.map((phone: Phone) => (
+  const phonesInput = phones.map(({ number }: Phone, idx) => (
     <input
-      onChange={handleChange}
+      onChange={(e) => changePhone(e, idx)}
       placeholder="phone number"
-      key={phone.number}
+      key={idx}
       type="tel"
-      value={phone.number}
+      value={number}
     />
   ));
+
+  const addPhoneInput = () => {
+    setPhones([...phones, { number: "" }]);
+  };
 
   return (
     <div>
       <h1>Form Contact</h1>
-      <form>
+      <div>
         <div>
           <input
             type="text"
-            onChange={handleChange}
+            onChange={changeFirstName}
             placeholder="first name"
-            value={first_name}
+            value={firstName}
           />
         </div>
         <div>
           <input
             type="text"
-            onChange={handleChange}
+            onChange={changeLastName}
             placeholder="last name"
-            value={last_name}
+            value={lastName}
           />
         </div>
-        <div>{phoneList}</div>
+        <div>
+          {phonesInput}
+          <button onClick={addPhoneInput}>+</button>
+        </div>
         <div>
           <Link to="/">cancel</Link>
           <input type="button" value="save" />
         </div>
-      </form>
+      </div>
     </div>
   );
 };
